01cf9fe1970b3e3ba43b2fcf5d4359efebe55cd2
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-02 01:23:52 +0530

    Update warns_sql.py

--- VisaRobot/modules/sql/warns_sql.py
+++ VisaRobot/modules/sql/warns_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from AsunaRobot.modules.sql import BASE, SESSION
+from VisaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 from sqlalchemy.dialects import postgresql
 
