2d3c1345f805bb8d7c453232498033ab247ec18e
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-02 01:19:40 +0530

    Update notes_sql.py

--- VisaRobot/modules/sql/notes_sql.py
+++ VisaRobot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from AsunaRobot.modules.helper_funcs.msg_types import Types
-from AsunaRobot.modules.sql import BASE, SESSION
+from VisaRobot.modules.helper_funcs.msg_types import Types
+from VisaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
 
