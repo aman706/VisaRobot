187cb60c0175d8cfe28e3c6beb4f38e3bb447ab7
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-02 01:20:07 +0530

    Update reporting_sql.py

--- VisaRobot/modules/sql/reporting_sql.py
+++ VisaRobot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from AsunaRobot.modules.sql import BASE, SESSION
+from VisaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
