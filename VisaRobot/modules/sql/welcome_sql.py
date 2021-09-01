87171bb9416c0d95eeeb02a2d5b1bc750718cded
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-02 01:24:41 +0530

    Update welcome_sql.py

--- VisaRobot/modules/sql/welcome_sql.py
+++ VisaRobot/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from AsunaRobot.modules.helper_funcs.msg_types import Types
-from AsunaRobot.modules.sql import BASE, SESSION
+from VisaRobot.modules.helper_funcs.msg_types import Types
+from VisaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
