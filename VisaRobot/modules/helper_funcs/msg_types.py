984b7a1de78d5c2ca782cc8253fb71fac3bdc10e
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-01 22:56:49 +0530

    Update msg_types.py

--- VisaRobot/modules/helper_funcs/msg_types.py
+++ VisaRobot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from AsunaRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from VisaRobot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
