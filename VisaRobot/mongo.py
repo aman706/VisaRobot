17be5c792fc48646460a7417fab59f68db242ad8
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-01 22:18:00 +0530

    Update mongo.py

--- VisaRobot/mongo.py
+++ VisaRobot/mongo.py
@@ -3,22 +3,22 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from AsunaRobot import MONGO_DB_URI 
+from VisaRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from AsunaRobot.conf import get_int_key, get_str_key
+from VisaRobot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "DaisyX"
+MONGO_DB = "VisaRobot"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["AsunaRobot"]
+db = client["VisaRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
