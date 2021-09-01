44f68f07f46bb6f0cb7c00b56b3a33c593b3afd7
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-01 22:50:20 +0530

    Update chat_status.py

--- VisaRobot/modules/helper_funcs/chat_status.py
+++ VisaRobot/modules/helper_funcs/chat_status.py
@@ -2,7 +2,7 @@ from time import perf_counter
 from functools import wraps
 from cachetools import TTLCache
 from threading import RLock
-from AsunaRobot import (
+from VisaRobot import (
     DEL_CMDS,
     DEV_USERS,
     DRAGONS,
@@ -402,6 +402,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from AsunaRobot.modules import connection
+from VisaRobot.modules import connection
 
 connected = connection.connected
