a4381c4a28a1b643a5b798da06f460ac06dcb78a
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-01 23:13:11 +0530

    Update forceSubscribe_sql.py

--- VisaRobot/modules/sql_extended/forceSubscribe_sql.py
+++ VisaRobot/modules/sql_extended/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from AsunaRobot.modules.sql import BASE, SESSION
+from VisaRobot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
