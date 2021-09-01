94906690a3d8d8f89d56f020dfd7da2cdc3bc7a7
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-02 01:21:17 +0530

    Update rules_sql.py

--- VisaRobot/modules/sql/rules_sql.py
+++ VisaRobot/modules/sql/rules_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from AsunaRobot.modules.sql import BASE, SESSION
+from VisaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Column, String, UnicodeText, distinct, func
 
 
