4a951e24c3e3d117b133e29fdb75d97467f366f3
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-01 23:14:01 +0530

    Update night_mode_sql.py

--- VisaRobot/modules/sql_extended/night_mode_sql.py
+++ VisaRobot/modules/sql_extended/night_mode_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from AsunaRobot.modules.sql import BASE, SESSION
+from VisaRobot.modules.sql import BASE, SESSION
 
 
 class Nightmode(BASE):
